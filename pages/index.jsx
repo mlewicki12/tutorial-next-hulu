9de477421016d72f071c5f599c2cfc3a4736ea87
Maciej Lewicki <lewickimapl@gmail.com>
2022-02-06 17:12:18 +0100

    create navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 
 import Head from 'next/head'
+
 import Header from '../components/header'
+import Nav from '../components/nav'
 
 export default function Home() {
   return (
@@ -10,10 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      { /** header */ }
       <Header />
-      
       { /** navbar */ }
+      <Nav />
 
       { /** results */ }
     </div>
